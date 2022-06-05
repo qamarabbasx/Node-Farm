b6e81fac75a0d2f5c37ef36c93188497490d63dd
Qamar Abbas <reply.qamar@gmail.com>
2022-06-05 14:33:14 +0500

    Overview and card pages has been updated dynamicaaly,real data instead of placeholders

--- index.js
+++ index.js
@@ -32,19 +32,44 @@ console.log('Will Read this code first then Asynchronous ❤  ');
 
 ////////////////////////////////////////////////////////////
 // Server
+const replaceTemplate = (temp,product) =>{
+    let output=temp.replace(/{%PRODUCTNAME%}/g,product.productName);
+    output=output.replace(/{%IMAGE%}/g,product.image);
+    output=output.replace(/{%FROM%}/g,product.from);
+    output=output.replace(/{%NUTRIENTS%}/g,product.nutrients);
+    output=output.replace(/{%QUANTITY%}/g,product.quantity);
+    output=output.replace(/{%PRICE%}/g,product.price);
+    output=output.replace(/{%DESCRIPTION%}/g,product.description);
+    output=output.replace(/{%ID%}/g,product.id);
+
+    if(!product.organic) output=output.replace(/{%NOT_ORGANIC%}/g,'not-organic');
+    return output;
+}
+const tempOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html`,'utf-8');
+const tempProduct = fs.readFileSync(`${__dirname}/templates/template-product.html`,'utf-8');
+const tempCard = fs.readFileSync(`${__dirname}/templates/template-card.html`,'utf-8');
+
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`,'utf-8');
 const dataObj= JSON.parse(data);
 
 const server= http.createServer((req,res)=>{
     const pathName=req.url;
+
+    // Overview Page
     if(pathName === '/' || pathName === '/overview'){
-        res.end('This is Overview Tab!');
+        res.writeHead(200,{'content-type' :'text/html'});
+        const cardsHtml = dataObj.map(el=>replaceTemplate(tempCard,el)).join('');
+        const output = tempOverview.replace('{%PRODUCT_CARDS%}',cardsHtml);
+        res.end(output);
     }
+    // Products Page
     else if (pathName === '/products'){
         res.end('This is Products Page!');
+        // API
     }else if(pathName === '/api'){
         res.writeHead(200,{'content-type' :'application/json'});
             res.end(data);
+            // Not Found
     }else{
         res.writeHead(404,{
             'Content-type':'text/html',
